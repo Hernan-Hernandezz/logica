3a615a57def2a1dfdd4042d78726bf94cc4b880a
Hernan-Hernandez <andreshernandez0827@gmail.com>
2020-06-08 03:56:58 -0500

    agregue busqueda binaria

---
+++ busqueda_binaria.py
@@ -0,0 +1,13 @@
+objetivo = int(input(f'escoge un numero'))
+epsilon = 0.0001
+bajo = 0.0
+alto = max(1.0,objetivo)
+respuesta = (alto+bajo)/2
+while abs(respuesta**2-objetivo) >= epsilon :
+	if respuesta**2 < objetivo:
+		bajo = respuesta
+	else :
+		alto = respuesta
+	respuesta = (alto+bajo) / 2
+print(f'la raiz cuadrada de {objetivo} es {respuesta}')
+
