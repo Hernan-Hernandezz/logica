9a98f3497ecf66b166a3c809cc00721640d26db0
Hernan-Hernandez <andreshernandez0827@gmail.com>
2020-06-08 23:53:12 -0500

    funcion raiz completa

--- funcion_raiz.py
+++ funcion_raiz.py
@@ -1,25 +1,36 @@
+def raiz(operacion,objetivo):
+	if operacion == 1:
 #enumeracion exaustiva
-	objetivo = int(input(f'escoge un numero : '))
-	epsilon = 0.01
-	paso = epsilon**2
-	respuesta = 0.0
+		objetivo = objetivo
+		epsilon = 0.01
+		paso = epsilon**2
+		respuesta = 0.0
 
-	while abs(respuesta**2 - objetivo)>=epsilon and respuesta >
-        	respuesta += paso
-	if abs(respuesta**2 - objetivo) >= epsilon:
-        	print(f'no se encontro raiz cuadrada de {objetivo}>
-	else :
-        	print(f'la raiz cuadrada de {objetivo} es {respues>
+		while abs(respuesta**2 - objetivo)>=epsilon and respuesta <= objetivo:
+        		respuesta += paso
+		if abs(respuesta**2 - objetivo) >= epsilon:
+        		print(f'no se encontro raiz cuadrada de {objetivo}')
+		else :
+        		print(f'la raiz cuadrada de {objetivo} es {respuesta}')
+		print('operacion 1')
+	elif operacion == 2:
 #busqueda binaria
-	objetivo = int(input(f'escoge un numero'))
-	epsilon = 0.0001
-	bajo = 0.0
-	alto = max(1.0,objetivo)
-	respuesta = (alto+bajo)/2
-	while abs(respuesta**2-objetivo) >= epsilon :
-        	if respuesta**2 < objetivo:
-                	bajo = respuesta
-        	else :
-                	alto = respuesta
-        	respuesta = (alto+bajo) / 2
-	print(f'la raiz cuadrada de {objetivo} es {respuesta}')
+		objetivo = objetivo
+		epsilon = 0.0001
+		bajo = 0.0
+		alto = max(1.0,objetivo)
+		respuesta = (alto+bajo)/2
+		while abs(respuesta**2-objetivo) >= epsilon :
+        		if respuesta**2 < objetivo:
+                		bajo = respuesta
+        		else :
+                		alto = respuesta
+        		respuesta = (alto+bajo) / 2
+		print(f'la raiz cuadrada de {objetivo} es {respuesta}')
+		print('operacion 2')
+	else:
+		print('error')
+
+raiz(1,16)
+raiz(2,16)
+raiz(3,16)
