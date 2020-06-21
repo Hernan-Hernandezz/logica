c5cf2b4432fa932f8cfbb3fe09adfd09124e79ac
Hernan Hernandez <andreshernandez0827@gmail.com>
2020-06-21 00:17:39 -0500

    caja negra modificado

--- CajaNegra.py
+++ CajaNegra.py
@@ -7,18 +7,10 @@ class CajaNegraTest(unittest.TestCase):
 		num_2 = 5
 		resultado = suma( num_1 , num_2 )
 		self.assertEqual(resultado , 15 )
-<<<<<<< HEAD
-	def teste_suma_dos_negativos(logi):
-		num_1 = -10
-		num_2 = -5
-		resultado = suma( num_1 , num_2 )
-		logi.assertEqual(resultado , -15 )
-=======
 	def teste_suma_dos_negativos(self):
 		num_1 = -10
 		num_2 = -5
 		resultado = suma( num_1 , num_2 )
 		self.assertEqual(resultado , -15 )
->>>>>>> deeb135672fceec9f24273409adf2e58cce9da8b
 if __name__ == '__main__' :
 	unittest.main()
