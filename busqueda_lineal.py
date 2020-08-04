d1444bf67df026cc3051fa13613caad245374535
Hernan_Hernandezz <andreshernandez0827@gmail.com>
2020-08-04 11:09:00 -0500

    nuevo archivo:  busqueda_binaria_n.py 	renombrado:     busqueda_binaria.py -> busqueda_binaria_raiz.py 	nuevo archivo:  busqueda_lineal.py 	nuevo archivo:  prueba.py

---
+++ busqueda_lineal.py
@@ -0,0 +1,19 @@
+import random
+
+def busqueda_lineal(lista,objetivo):
+
+	match = False
+	for elemento in lista:
+		if elemento == objetivo:
+			match = True
+			break
+	return match
+
+if __name__=='__main__':
+	tamano_lista = int(input('cual es el tamano de tu lista?'))
+	objetivo = int(input('que numero quieres encontrar'))
+
+	lista =[random.randint(0,100)for i in range(tamano_lista)]
+	encontrado = busqueda_lineal(lista,objetivo)
+	print(lista)
+	print(f'El elemento {objetivo} {"esta" if encontrado else "no esta"} en la lista')
