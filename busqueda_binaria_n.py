e88acebf0493717a42a75e121b85b516b38cc3ae
Hernan_Hernandezz <andreshernandez0827@gmail.com>
2020-08-05 10:18:38 -0500

    modificado:     busqueda_binaria_n.py 	arregle problemas en mi archivo

--- busqueda_binaria_n.py
+++ busqueda_binaria_n.py
@@ -1,25 +1,27 @@
 import random
 
 def busqueda_binaria(lista,comienzo,final,objetivo):
-	print(f'encontrar numero {objetivo} entre {comienzo} y {final}')
-
-	if comienzo >final :
-
+	print(f'encontrar numero {objetivo} entre {lista[comienzo]} y {lista[final - 1]}')
+	if comienzo > final :
 		return False
-	medio = ( comienzo + final ) //	2
 
-	if lista[medio] == objectivo :
+	medio = ( comienzo + final ) //	2
+	
+	if lista[medio] == objetivo :
 		return True
 	elif lista[medio] < objetivo :
-		busqueda_binaria( lista, medio + 1, final , objetivo )
+		return busqueda_binaria( lista, medio + 1, final , objetivo )
 	else:
-		busqueda_binaria( lista, comienzo, medio -1, objetivo)
+		return busqueda_binaria( lista, comienzo, medio -1, objetivo)
+
+
 if __name__=='__main__':
 	tamano_lista = int(input('cual es el tamano de tu lista?'))
 	objetivo = int(input('que numero quieres encontrar'))
 
-	lista =[(random.randint( 0, 100)) for i in range(tamano_lista)]
-	lista = sorted(lista)
+	lista =sorted([random.randint(0,100) for i in range(tamano_lista)])
+	
 	encontrado = busqueda_binaria(lista, 0, len(lista), objetivo)
+	
 	print(lista)
 	print(f'El elemento {objetivo} {"esta" if encontrado else "no esta"} en la lista')
